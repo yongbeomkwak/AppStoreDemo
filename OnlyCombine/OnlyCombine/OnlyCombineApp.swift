668f60c43e753244d55a096ced479924ef771d8e
Hamp <yongbeomkwak@gmail.com>
2024-07-12 22:45:37 +0900

    :tada: :: Only Combine 프로젝트

---
+++ OnlyCombine/OnlyCombine/OnlyCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyCombineApp.swift
+//  OnlyCombine
+//
+//  Created by yongbeomkwak on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlyCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnlyCombine/OnlyCombineUITests/OnlyCombineUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  OnlyCombineUITestsLaunchTests.swift
+//  OnlyCombineUITests
+//
+//  Created by yongbeomkwak on 7/12/24.
+//
+
+import XCTest
+
+final class OnlyCombineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
